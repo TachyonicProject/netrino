424b8c767bedadaea80f7f4ebd8226b35d4127b3
Vuader <dave@tachyonic.co.za>
2019-02-04 13:13:05 +0200

    https://github.com/TachyonicProject/infinitystone/issues/66 Moved Elements back to Infinitystone

--- netrino/views/__init__.py
+++ netrino/views/__init__.py
@@ -3,6 +3,5 @@ from netrino.views import interface
 from netrino.views import service_templates
 from netrino.views import service_requests
 from netrino.views import yang
-from netrino.views import elements
 from netrino.views import resources
 from netrino.views import openstack_proxy
