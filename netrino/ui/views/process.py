5829ff6607b75cf34dfaba3b41410e3b62dc820a
Christiaan Frans Rademan <christiaan.rademan@gmail.com>
2019-03-25 11:28:01 +0200

    updates fixes

--- netrino/ui/views/process.py
+++ netrino/ui/views/process.py
@@ -31,11 +31,14 @@ from luxon import g
 from luxon import router
 from luxon import register
 from luxon import render_template
-from luxon.constants import TEXT_HTML
+from luxon.utils.bootstrap4 import form
+
+from netrino.ui.models.processes import netrino_process
 
 g.nav_menu.add('/Infrastructure/Orchestration/Process Manager',
                href='/infrastructure/orchestration/process',
-               tag='infrastructure:admin',
+               tag='process:admin',
+               endpoint='orchestration',
                feather='grid')
 
 
@@ -43,11 +46,80 @@ g.nav_menu.add('/Infrastructure/Orchestration/Process Manager',
 class ProcessManager():
     def __init__(self):
         router.add('GET',
+                   '/infrastructure/orchestration/process',
+                   self.list,
+                   tag='process:view')
+
+        router.add('GET',
                    '/infrastructure/orchestration/process/{process_id}',
-                   self.edit)
+                   self.edit,
+                   tag='process:admin')
+
+        router.add('POST',
+                   '/infrastructure/orchestration/process/edit/{process_id}',
+                   self.edit,
+                   tag='process:admin')
+
+        router.add('GET',
+                   '/infrastructure/orchestration/process/delete/{process_id}',
+                   self.delete,
+                   tag='process:admin')
+
+        router.add(('GET', 'POST',),
+                   '/infrastructure/orchestration/process/add',
+                   self.add,
+                   tag='process:admin')
+
+        router.add(('GET', 'PUT',),
+                   '/infrastructure/orchestration/process/{process_id}/{node}',
+                   self.node,
+                   tag='process:admin')
+
+    def list(self, req, resp):
+        return render_template('netrino.ui/process/list.html',
+                               view='Process Manager')
+
+    def delete(self, req, resp, process_id):
+        req.context.api.execute('DELETE', '/v1/process/%s' % process_id,
+                                endpoint='orchestration')
+        return self.list(req, resp)
 
     def edit(self, req, resp, process_id):
-        resp.content_type = TEXT_HTML
-        return render_template('netrino.ui/process/edit.html',
+        if req.method == 'POST':
+            data = req.form_dict
+            req.context.api.execute('PUT', '/v1/process/%s' % process_id,
+                                    data=data,
+                                    endpoint='orchestration')
+            req.method = 'GET'
+            return self.edit(req, resp, process_id)
+        else:
+            proc = req.context.api.execute('GET',
+                                           '/v1/process/%s' % process_id,
+                                           endpoint='orchestration')
+
+            html_form = form(netrino_process, proc.json)
+
+            return render_template('netrino.ui/process/edit.html',
+                                   form=html_form,
+                                   process_id=process_id,
+                                   view="Process Editor")
+
+    def node(self, req, resp, process_id, node):
+        return render_template('netrino.ui/process/node.html',
                                process_id=process_id,
-                               view="Process Editor")
+                               view="Node Editor")
+
+    def add(self, req, resp):
+        if req.method == 'POST':
+            data = req.form_dict
+            response = req.context.api.execute('POST', '/v1/process',
+                                               data=data,
+                                               endpoint='orchestration')
+
+            return self.edit(req, resp, response.json['id'])
+        else:
+            html_form = form(netrino_process)
+
+            return render_template('netrino.ui/process/add.html',
+                                   view='New Process',
+                                   form=html_form)
--- netrino/views/process.py
+++ netrino/views/process.py
@@ -27,40 +27,120 @@
 # CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
 # THE POSSIBILITY OF SUCH DAMAGE.
-from lxml import objectify
+from luxon import GetLogger
 from luxon import router
 from luxon import register
-from luxon.constants import APPLICATION_XML
+from luxon import db
+from luxon.constants import APPLICATION_XML, APPLICATION_JSON
+from luxon.helpers.api import sql_list, obj
+from luxon.exceptions import SQLIntegrityError, HTTPBadRequest
+
+from netrino.utils.mxgraph import MxChangeDecoder, mxgraph
+from netrino.models.processes import netrino_process
+
+log = GetLogger(__name__)
 
 
 @register.resources()
 class Workflow():
     def __init__(self):
-        router.add('GET',
-                   '/workflow/{process_id}',
-                   self.get_workflow)
+        router.add('GET', '/v1/process/{process_id}',
+                   self.get_process,
+                   tag='process:admin')
+        router.add('GET', '/v1/processes',
+                   self.get_processes,
+                   tag='process:view')
+        router.add('POST', '/v1/process',
+                   self.post_process,
+                   tag='process:admin')
+        router.add(['PUT', 'PATCH'], '/v1/process/{process_id}',
+                   self.put_process,
+                   tag='process:admin')
+        router.add('DELETE', '/v1/process/{process_id}',
+                   self.delete_process,
+                   tag='process:admin')
 
+        router.add('GET',
+                   '/v1/workflow/{process_id}',
+                   self.get_workflow,
+                   tag='process:admin')
         router.add('PUT',
-                   '/workflow/{process_id}',
-                   self.update_workflow)
+                   '/v1/workflow/{process_id}',
+                   self.update_workflow,
+                   tag='process:admin')
+
+    def get_process(self, req, resp, process_id):
+        return obj(req, netrino_process, sql_id=process_id)
+
+    def get_processes(self, req, resp):
+        return sql_list(req, 'netrino_process',
+                        ('id', 'name',),)
+
+    def post_process(self, req, resp):
+        process = obj(req, netrino_process)
+        process.commit()
+        with db() as conn:
+            node_id = "1/%s" % process['id']
+            sql = "INSERT INTO netrino_workflow"
+            sql += " (id, process_id, node, node_id, node_type, updated_time)"
+            sql += " VALUES"
+            sql += " (uuid(), %s, 1, %s, 'root', now())"
+            conn.execute(sql, (process['id'], node_id,))
+            conn.commit()
+
+        return process
+
+    def put_process(self, req, resp, process_id):
+        user = obj(req, netrino_process, sql_id=process_id)
+        user.commit()
+        return user
+
+    def delete_process(self, req, resp, process_id):
+        user = obj(req, netrino_process, sql_id=process_id)
+        user.commit()
 
     def get_workflow(self, req, resp, process_id):
         resp.content_type = APPLICATION_XML
-        empty = """<mxGraphModel>
-    <root>
-        <Workflow label="MyWorkflow" description="" id="0"/>
-        <Layer label="Default Layer" description="">
-            <mxCell parent="0"/>
-        </Layer>
-    </root>
-</mxGraphModel>"""
-        graph = """
-<mxGraphModel><root><Workflow label="MyWorkflow" description="" id="0"><mxCell/></Workflow><Layer label="Default Layer" description="" id="1"><mxCell parent="0"/></Layer><Task label="Task" description="" href="" id="11"><mxCell vertex="1" parent="1"><mxGeometry x="150" y="100" width="72" height="32" as="geometry"/></mxCell></Task><Task label="Task" description="" href="" id="12"><mxCell vertex="1" parent="1"><mxGeometry x="300" y="160" width="72" height="32" as="geometry"/></mxCell></Task><Edge label="" description="" id="13"><mxCell style="straightEdge" edge="1" parent="1" source="11" target="12"><mxGeometry relative="1" as="geometry"/></mxCell></Edge><Task label="Task" description="" href="" id="14"><mxCell vertex="1" parent="1"><mxGeometry x="80" y="180" width="72" height="32" as="geometry"/></mxCell></Task><Edge label="" description="" id="15"><mxCell style="straightEdge" edge="1" parent="1" source="11" target="14"><mxGeometry relative="1" as="geometry"/></mxCell></Edge><Task label="Task" description="" href="" id="16"><mxCell vertex="1" parent="1"><mxGeometry x="210" y="240" width="72" height="32" as="geometry"/></mxCell></Task><Edge label="" description="" id="17"><mxCell edge="1" parent="1" source="14" target="16"><mxGeometry relative="1" as="geometry"/></mxCell></Edge></root></mxGraphModel>
-"""
-        return graph
+        with db() as conn:
+            conn.execute('DELETE FROM netrino_workflow' +
+                         ' WHERE process_id = %s' +
+                         ' AND (node_removed = 1' +
+                         ' OR node_type is NULL)', process_id)
+            conn.commit()
+            graph = conn.execute('SELECT * FROM netrino_workflow' +
+                                 ' WHERE process_id = %s' +
+                                 ' AND node_removed = 0',
+                                 process_id).fetchall()
+            conn.commit()
+            return mxgraph(graph)
 
     def update_workflow(self, req, resp, process_id):
         resp.content_type = APPLICATION_XML
-        objectify.fromstring(req.read())
-
+        decoder = MxChangeDecoder(req.read(), process_id)
+        changes = decoder.parse()
+        with db() as conn:
+            try:
+                for change in changes:
+                    values = [process_id]
+                    sql = "INSERT INTO netrino_workflow ("
+                    sql += "id"
+                    sql += ", process_id"
+                    sql += ", updated_time"
+                    for attr in change:
+                        sql += ", %s" % attr
+                        values.append(change[attr])
+                    sql += ") VALUES ("
+                    sql += "uuid(), %s, now()"
+                    for attr in change:
+                        sql += ", %s"
+                    sql += ") ON DUPLICATE KEY UPDATE"
+                    sql += " process_id = %s,"
+                    sql += " updated_time = now()"
+                    for attr in change:
+                        sql += ", %s = %s" % (attr, '%s',)
+                    conn.execute(sql, values+values)
+                conn.commit()
+            except SQLIntegrityError:
+                resp.content_type = 'APPLICATION_JSON'
+                raise HTTPBadRequest('Graph modified and not in sync')
         return None
